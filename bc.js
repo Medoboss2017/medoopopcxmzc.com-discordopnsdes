90cad003c6921beafb8ee25e27685ee405d18b6a
Medoboss2017 <44714458+Medoboss2017@users.noreply.github.com>
2018-11-10 01:39:10 +0200

    Update bc.js

--- bc.js
+++ bc.js
@@ -47,5 +47,34 @@ client.on('message',async message => {
     }
   });
  
+client.on('message', message => {
+  var prefix = ".";
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "307630049656438788") return;
+
+if (message.content.startsWith(prefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(Playing: **${argresult})
+} 
+
+if (message.content.startsWith(prefix + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/v5bz%22);
+     console.log('test' + argresult);
+    message.channel.sendMessage(Streaming: **${argresult})
+} 
+
+if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(Username Changed To **${argresult}**)
+  return message.reply("You Can change the username 2 times per hour");
+} 
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(Avatar Changed Successfully To **${argresult}**);
+}
+});
+
 client.login(process.env.BOT_TOKEN);
 
